8709bc6346bcee517e5c4bf0872f54f46698323b
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-12-11 17:02:22 +0100

    Removed unneeded content, moved function definition

--- src/Scripts/Meta/FileHandling.lua
+++ src/Scripts/Meta/FileHandling.lua
@@ -8,3 +8,15 @@ if mod.hadesGameFolder == "root" then
 	local hadesTwoContentFolder = rom.paths.Content()
 	mod.hadesGameFolder = rom.path.combine(rom.path.get_parent(rom.path.get_parent(hadesTwoContentFolder)), "Hades")
 end
+
+-- Loads RoomData from a file in Hades
+function mod.LoadHadesRoomData(fileName)
+	local pathName = rom.path.combine(mod.hadesGameFolder, "Content/Scripts", fileName)
+	local chunk, err = loadfile(pathName)
+	if chunk then
+		chunk()
+		return RoomSetData
+	else
+		print("Error loading RoomData: " .. err)
+	end
+end
