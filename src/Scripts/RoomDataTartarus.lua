2311f474beb556d7b323a1a92b07b01c8db586ac
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-12-23 15:54:17 +0100

    Removed TODO

--- src/Scripts/RoomDataTartarus.lua
+++ src/Scripts/RoomDataTartarus.lua
@@ -1,14 +1,16 @@
--- TODO: Collision in Tartarus rooms doesn't work (pillars, always broken), traps no longer triggering (since Tartarus encounters added - logic broken that checks if they should be active or not?) 
+-- Adds RoomData for Tartarus from Hades to Hades II
 
 local roomSetDataTartarus = mod.LoadHadesRoomData("RoomDataTartarus.lua").Tartarus
 local roomModifications = {
 	BaseTartarus = {
+		-- For debugging, toggle this to toggle Tartarus or Erebus encounters
 		-- LegalEncounters = game.EncounterSets.FEncountersDefault,
 		SaveProfileLocationText = roomSetDataTartarus.BaseTartarus.LocationText,
 	},
 	RoomOpening = {
+		-- For debugging, switch between these two to toggle Tartarus or Erebus encounters
 		LegalEncounters = { "OpeningGenerated" },
-		-- LegalEncounters = { "OpeningEmpty", "OpeningGeneratedF" },
+		-- LegalEncounters = { "OpeningGeneratedF" },
 		EntranceFunctionArgs = { HeroGoalAngle = 335 },
 		UnthreadedEvents = {},
 		ThreadedEvents = {
