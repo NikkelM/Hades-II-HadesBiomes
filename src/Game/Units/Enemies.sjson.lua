08c8a737538cfd1ebe98e9527ea402e345b647c2
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-12-29 19:21:51 +0100

    Add GrannyModel to enemies

--- src/Game/Units/Enemies.sjson.lua
+++ src/Game/Units/Enemies.sjson.lua
@@ -1,9 +1,39 @@
 -- Adds enemies from Hades to Hades II
 
+-- Applies modifications to the given table
+local function applyModifications(enemiesTable, modifications)
+	for _, enemy in ipairs(enemiesTable) do
+		local modification = modifications[enemy.Name]
+		if modification then
+			for key, value in pairs(modification) do
+				if type(value) == "table" then
+					enemy[key] = enemy[key] or {}
+					for subKey, subValue in pairs(value) do
+						enemy[key][subKey] = subValue
+					end
+				else
+					enemy[key] = value
+				end
+			end
+		end
+	end
+end
+
 local hadesEnemiesFile = rom.path.combine(mod.hadesGameFolder, "Content\\Game\\Units\\Enemies.sjson")
 local hadesEnemiesTable = sjson.decode_file(hadesEnemiesFile)
 
 local hadesTwoEnemiesFile = rom.path.combine(rom.paths.Content(), "Game\\Units\\Enemies.sjson")
+
+-- Modifications/overrides to the Hades enemies
+local hadesEnemiesModifications = {
+	BaseGlutton = {
+		Thing = {
+			GrannyModel = "Blank_Mesh"
+		}
+	}
+}
+
+applyModifications(hadesEnemiesTable, hadesEnemiesModifications)
 
 sjson.hook(hadesTwoEnemiesFile, function(data)
 	mod.AddTableKeysSkipDupes(data.Units, hadesEnemiesTable.Units, "Name")
