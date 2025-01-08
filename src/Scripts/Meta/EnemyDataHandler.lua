65c2fc63f1395f23b88be480dc3000da4ab655d2
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2025-01-08 14:04:17 +0000

    Moved trap replacements to own file

--- src/Scripts/Meta/EnemyDataHandler.lua
+++ src/Scripts/Meta/EnemyDataHandler.lua
@@ -83,66 +83,8 @@ for oldName, newName in pairs(EnemyWeaponMappings) do
 end
 
 -- Some enemies need to be modified so much, it's easier to redefine them
--- TODO: Move to own file for brevity
--- We need to edit the original trap/enemy instead of adding a new one, as otherwise the maps won't populate the correct trap
--- AFAIK, the original SpikeTrap is not used anywhere in Hades II at the moment
-game.EnemyData.SpikeTrap.WeaponOptions = { "HadesSpikeTrapWeapon" }
-game.EnemyData.SpikeTrap.DefaultAIData.IdleAnimation = "HadesSpikeTrapIdle"
-game.EnemyData.SpikeTrap.DefaultAIData.PreAttackAnimation = "HadesSpikeTrapPreFire"
-game.EnemyData.SpikeTrap.DefaultAIData.PostAttackAnimation = "HadesSpikeTrapPressed"
-game.EnemyData.SpikeTrap.DefaultAIData.DisabledAnimation = "HadesSpikeTrapDeactivated"
-
-enemyData.DartTrap = {
-	InheritFrom = { "BaseTrap" },
-	TargetGroups = { "GroundEnemies", "HeroTeam" },
-	-- All of these properties were in the root before, but need to be in DefaultAIData for Hades II
-	DefaultAIData = {
-		DeepInheritance = true,
-		PreAttackDuration = 0.2,
-		PostAttackCooldown = 2.0,
-		LinkedEnemy = "DartTrapEmitter",
-		IdleAnimation = "DartTrapIdle",
-		PreAttackAnimation = "DartTrapPreFire",
-		PreAttackSound = "/SFX/TrapSet",
-		PostAttackAnimation = "DartTrapPressed",
-		ReloadingLoopSound = "/SFX/TrapSettingLoop",
-		ReloadedSound = "/Leftovers/Menu Sounds/TalismanMetalClankDown",
-		DisabledAnimation = "DartTrapDeactivated",
-		AttackDistance = 100,
-		AIResetDistance = 110,
-		MaxVictimZ = 1,
-	},
-	AIOptions =
-	{
-		-- RemoteAI
-		"RemoteAttackModsNikkelMHadesBiomes"
-	},
-	ToggleTrap = true,
-}
--- Fires when the player steps on a Dart trap plate
-enemyData.DartTrapEmitter = {
-	InheritFrom = { "IsNeutral" },
-	Type = "Trap",
-	TriggersOnHitEffects = false,
-	DefaultAIData = {
-		DeepInheritance = true,
-		PreAttackAnimation = "DartTrapEmitterFire",
-		PostAttackAnimation = "DartTrapEmitterReturnToIdle",
-		PreAttackDuration = 0.0,
-		PostAttackDuration = 0.0,
-		FireTicksMin = 3,
-		FireTicksMax = 3,
-		FireInterval = 0.15,
-		TrackTargetDuringCharge = false,
-		-- Manually added!
-		ProjectileName = "DartTrapWeapon",
-		PostAttackCooldown = 2.0,
-	},
-	Material = "MetalObstacle",
-	WeaponOptions = { "DartTrapWeapon" },
-	WeaponName = "DartTrapWeapon",
-	OutgoingDamageModifiers = { { NonPlayerMultiplier = 33.33 } }
-}
+-- This is true for most traps
+mod.ModifyEnemyTrapData(enemyData)
 
 local enemyReplacements = {}
 
