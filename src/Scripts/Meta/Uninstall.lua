5f50fa2400aa9880b17382b638f995228b6a93a0
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-01-31 18:13:03 +0000

    Refactored code

--- src/Scripts/Meta/Uninstall.lua
+++ src/Scripts/Meta/Uninstall.lua
@@ -1,13 +1,46 @@
+local function removeFiles(fileMappings, basePath, fileType)
+	mod.DebugPrint("Removing " .. fileType .. " files...", 3)
+	for src, dest in pairs(fileMappings) do
+		local destPath = rom.path.combine(rom.paths.Content(), basePath .. dest)
+		if rom.path.exists(destPath) then
+			mod.DebugPrint("Removing file: " .. destPath, 4)
+			local success, err = os.remove(destPath)
+			if not success then
+				mod.DebugPrint("Error removing file: " .. err, 1)
+			end
+		end
+	end
+end
+
+local function removeFile(filePath, fileType)
+	if rom.path.exists(filePath) then
+		mod.DebugPrint("Removing file: " .. filePath, 4)
+		local success, err = os.remove(filePath)
+		if not success then
+			mod.DebugPrint("Error removing file: " .. err, 1)
+		end
+	end
+end
+
+local function removeFilesByNames(fileNames, basePath, extension, fileType)
+	mod.DebugPrint("Removing " .. fileType .. " files...", 3)
+	for _, name in ipairs(fileNames) do
+		local destPath = rom.path.combine(rom.paths.Content(), basePath .. name .. extension)
+		removeFile(destPath, fileType)
+	end
+end
+
 function mod.Uninstall()
 	-- Check if any savegame has a modded run as the most recent one - block uninstallation if so, as it would corrupt the save file
 	local cachedRuns = mod.LoadCachedRunsFile()
 	if config.uninstall == "I AM SURE - UNINSTALL" then
-		mod.DebugPrint("Uninstalling mod - forced uninstallation! Savegames may corrupt. Re-install the mod to try and restore them.", 2)
+		mod.DebugPrint(
+			"Uninstalling mod - forced uninstallation! Savegames may corrupt. Re-install the mod to try and restore them.", 2)
 	else
 		for saveFileIndex, isActive in pairs(cachedRuns.ActiveModdedRuns) do
 			if isActive then
 				mod.DebugPrint(
-				"For at least one of your save files, the most recent run is/was a modded Hades run. The mod will not uninstall, otherwise your savegame will corrupt! Make sure you are currently in a normal Hades II run, or were in one before you returned to the crossroads! Then try uninstalling the mod again. Are you SURE this is wrong? Set \"uninstall\" in the config to \"I AM SURE - UNINSTALL\".",
+					"For at least one of your save files, the most recent run is/was a modded Hades run. The mod will not uninstall, otherwise your savegame will corrupt! Make sure you are currently in a normal Hades II run, or were in one before you returned to the crossroads! Then try uninstalling the mod again. Are you SURE this is wrong? Set \"uninstall\" in the config to \"I AM SURE - UNINSTALL\".",
 					1)
 				config.uninstall = "false"
 				return false
@@ -16,101 +49,23 @@ function mod.Uninstall()
 		mod.DebugPrint("Uninstalling mod - removing files added by the mod", 3)
 	end
 
-	-- TODO: Refactor
-	mod.DebugPrint("Removing .bank files...", 3)
-	for src, dest in pairs(AudioFileMappings) do
-		local destPath = rom.path.combine(rom.paths.Content(), "Audio\\Desktop\\" .. dest)
-		if rom.path.exists(destPath) then
-			mod.DebugPrint("Removing file: " .. destPath, 4)
-			local success, err = os.remove(destPath)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
-	end
+	removeFiles(AudioFileMappings, "Audio\\Desktop\\", ".bank")
+	removeFiles(PackageFileMappings, "Packages\\", ".pkg")
+	removeFiles(BikFileMappings, "Movies\\", ".bik")
+	removeFiles(SjsonFileMappings, "Game\\", ".sjson")
 
-	mod.DebugPrint("Removing .pkg files...", 3)
-	for src, dest in pairs(PackageFileMappings) do
-		local destPath = rom.path.combine(rom.paths.Content(), "Packages\\" .. dest)
-		if rom.path.exists(destPath) then
-			mod.DebugPrint("Removing file: " .. destPath, 4)
-			local success, err = os.remove(destPath)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
-	end
-
-	mod.DebugPrint("Removing .bik files...", 3)
-	for src, dest in pairs(BikFileMappings) do
-		local destPath = rom.path.combine(rom.paths.Content(), "Movies\\" .. dest)
-		if rom.path.exists(destPath) then
-			mod.DebugPrint("Removing file: " .. destPath, 4)
-			local success, err = os.remove(destPath)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
-	end
-
-	mod.DebugPrint("Removing .sjson files...", 3)
-	for src, dest in pairs(SjsonFileMappings) do
-		local destPath = rom.path.combine(rom.paths.Content(), "Game\\" .. dest)
-		if rom.path.exists(destPath) then
-			mod.DebugPrint("Removing file: " .. destPath, 4)
-			local success, err = os.remove(destPath)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
-	end
 	-- FX file
 	local fxSjsonFilePath = rom.path.combine(rom.paths.Content(), mod.HadesFxDestinationFilename)
-	if rom.path.exists(fxSjsonFilePath) then
-		mod.DebugPrint("Removing file: " .. fxSjsonFilePath, 4)
-		local success, err = os.remove(fxSjsonFilePath)
-		if not success then
-			mod.DebugPrint("Error removing file: " .. err, 1)
-		end
-	end
+	removeFile(fxSjsonFilePath, ".sjson")
 
-	mod.DebugPrint("Removing .map_text files...", 3)
-	for _, name in ipairs(MapFileNames) do
-		local mapTextDest = "Maps\\" .. name .. ".map_text"
-		local destPath = rom.path.combine(rom.paths.Content(), mapTextDest)
-		if rom.path.exists(destPath) then
-			mod.DebugPrint("Removing file: " .. destPath, 4)
-			local success, err = os.remove(destPath)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
-	end
-
-	mod.DebugPrint("Removing .thing_bin files...", 3)
-	for _, name in ipairs(MapFileNames) do
-		local thingBinDest = "Maps\\bin\\" .. name .. ".thing_bin"
-		local destPath = rom.path.combine(rom.paths.Content(), thingBinDest)
-		if rom.path.exists(destPath) then
-			mod.DebugPrint("Removing file: " .. destPath, 4)
-			local success, err = os.remove(destPath)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
-	end
+	removeFilesByNames(MapFileNames, "Maps\\", ".map_text", ".map_text")
+	removeFilesByNames(MapFileNames, "Maps\\bin\\", ".thing_bin", ".thing_bin")
 
 	mod.DebugPrint("Removing help text files...", 3)
 	for _, language in ipairs(HelpTextLanguages) do
 		local helpTextFile = rom.path.combine(rom.paths.Content(),
 			'Game\\Text\\' .. language .. '\\HelpTextHades.' .. language .. '.sjson')
-		if rom.path.exists(helpTextFile) then
-			mod.DebugPrint("Removing file: " .. helpTextFile, 4)
-			local success, err = os.remove(helpTextFile)
-			if not success then
-				mod.DebugPrint("Error removing file: " .. err, 1)
-			end
-		end
+		removeFile(helpTextFile, ".sjson")
 	end
 
 	-- Set the content of checksumsDest to an empty string to prompt an installation on the next start if the mod is enabled again
