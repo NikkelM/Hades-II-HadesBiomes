00c7d1f34f9ab573d77d0cc887dd6cf342572eb7
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-02-08 16:25:10 +0000

    Added placeholder

--- src/Scripts/RoomSets.lua
+++ src/Scripts/RoomSets.lua
@@ -1,5 +1,4 @@
 local newRoomSets = {
-	-- Tartarus
 	Tartarus = {
 		-- Opening room for the very first run, forces Athena boon, no enemies
 		-- "RoomSimple01",
@@ -11,47 +10,51 @@ local newRoomSets = {
 
 		"A_PostBoss01",
 
-		"A_MiniBoss01",
-		"A_MiniBoss02",
-		"A_MiniBoss03",
-		"A_MiniBoss04",
+		-- "A_MiniBoss01",
+		-- "A_MiniBoss02",
+		-- "A_MiniBoss03",
+		-- "A_MiniBoss04",
 
 		"A_PreBoss01",
-		"A_Shop01",
+		-- "A_Shop01",
 
-		"A_Reprieve01",
-		"A_Reprieve01",
+		-- "A_Reprieve01",
+		-- "A_Reprieve01",
 
-		"A_Combat01",
-		"A_Combat02",
-		"A_Combat03",
-		"A_Combat04",
-		"A_Combat05",
-		"A_Combat06",
-		"A_Combat07",
-		"A_Combat08A",
-		"A_Combat08B",
-		"A_Combat09",
-		"A_Combat10",
-		"A_Combat11",
-		"A_Combat12",
-		"A_Combat13",
-		"A_Combat14",
-		"A_Combat15",
-		"A_Combat16",
-		"A_Combat17",
-		"A_Combat18",
-		"A_Combat19",
-		"A_Combat20",
-		"A_Combat21",
-		"A_Combat24",
-		"A_Combat25",
+		-- "A_Combat01",
+		-- "A_Combat02",
+		-- "A_Combat03",
+		-- "A_Combat04",
+		-- "A_Combat05",
+		-- "A_Combat06",
+		-- "A_Combat07",
+		-- "A_Combat08A",
+		-- "A_Combat08B",
+		-- "A_Combat09",
+		-- "A_Combat10",
+		-- "A_Combat11",
+		-- "A_Combat12",
+		-- "A_Combat13",
+		-- "A_Combat14",
+		-- "A_Combat15",
+		-- "A_Combat16",
+		-- "A_Combat17",
+		-- "A_Combat18",
+		-- "A_Combat19",
+		-- "A_Combat20",
+		-- "A_Combat21",
+		-- "A_Combat24",
+		-- "A_Combat25",
 		-- These two rooms have no RoomData table attached
 		-- "A_Combat22",
 		-- "A_Combat23",
 
 		-- "A_Story01",
-	}
+	},
+
+	Asphodel = {
+
+	},
 }
 
 for key, newRoomSet in pairs(newRoomSets) do
