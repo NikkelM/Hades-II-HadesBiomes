0bcecf55e7b3b01f12188b7af92a4f2fb0de50ec
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-02-15 14:51:27 +0000

    Better reward preview offsets

--- src/Scripts/RewardPresentation.lua
+++ src/Scripts/RewardPresentation.lua
@@ -9,7 +9,7 @@ modutil.mod.Path.Wrap("CreateDoorRewardPreview", function(base, exitDoor, chosen
 
 		index = index or 1
 
-		local modifiedProperties = {
+		local properties = {
 			doorIconOffsetX = 0,
 			doorIconOffsetY = 30,
 			doorIconScale = 0.85,
@@ -31,7 +31,7 @@ modutil.mod.Path.Wrap("CreateDoorRewardPreview", function(base, exitDoor, chosen
 				doorIconOffsetY = 55,
 			},
 			A_Combat10 = {
-				doorIconOffsetY = 30,
+				doorIconOffsetY = 35,
 			},
 			A_Combat11 = {
 				doorIconOffsetY = 35,
@@ -44,15 +44,19 @@ modutil.mod.Path.Wrap("CreateDoorRewardPreview", function(base, exitDoor, chosen
 			},
 			A_MiniBoss01 = {
 				doorIconOffsetY = 62,
+				overrideSkip = true,
 			},
 			A_MiniBoss02 = {
 				doorIconOffsetY = 62,
+				overrideSkip = true,
 			},
 			A_MiniBoss03 = {
 				doorIconOffsetY = 62,
+				overrideSkip = true,
 			},
 			A_MiniBoss04 = {
 				doorIconOffsetY = 62,
+				overrideSkip = true,
 			},
 			A_PreBoss01 = {
 				doorIconOffsetY = 20,
@@ -68,18 +72,34 @@ modutil.mod.Path.Wrap("CreateDoorRewardPreview", function(base, exitDoor, chosen
 			StackUpgrade = { doorIconScale = true },
 		}
 
-		local properties = { "doorIconOffsetX", "doorIconOffsetY", "doorIconScale" }
 		if offsetMappings[game.CurrentRun.CurrentRoom.Name] then
-			for _, property in ipairs(properties) do
-				if not skipModifiers[chosenRewardType] or (skipModifiers[chosenRewardType] and not skipModifiers[chosenRewardType][property]) then
-					modifiedProperties[property] = offsetMappings[game.CurrentRun.CurrentRoom.Name][property] or
-							modifiedProperties[property]
+			for property, value in pairs(properties) do
+				if offsetMappings[game.CurrentRun.CurrentRoom.Name].overrideSkip or not skipModifiers[chosenRewardType] or (skipModifiers[chosenRewardType] and not skipModifiers[chosenRewardType][property]) then
+					properties[property] = offsetMappings[game.CurrentRun.CurrentRoom.Name][property] or properties[property]
 				end
 			end
 		end
 
+		local additionalModifications = {
+			Devotion = {
+				doorIconOffsetY = -5,
+			},
+		}
+
+		-- Add the additional modifications for the chosen reward type (or multiply for scale)
+		if additionalModifications[chosenRewardType] then
+			for property, value in pairs(properties) do
+				properties[property] = properties[property] + (additionalModifications[chosenRewardType][property] or 0)
+			end
+		end
+
+		-- For rewards like Devotion that don't reuse the preview we build below
+		exitDoor.RewardPreviewOffsetX = properties.doorIconOffsetX
+		exitDoor.RewardPreviewOffsetY = properties.doorIconOffsetY
+		exitDoor.RewardPreviewOffsetZ = doorIconOffsetZ
+
 		if IsHorizontallyFlipped({ Id = exitDoor.ObjectId }) then
-			modifiedProperties.doorIconOffsetX = modifiedProperties.doorIconOffsetX * -1
+			properties.doorIconOffsetX = properties.doorIconOffsetX * -1
 			doorIconIsometricShiftX = doorIconIsometricShiftX * -1
 		end
 
@@ -94,14 +114,14 @@ modutil.mod.Path.Wrap("CreateDoorRewardPreview", function(base, exitDoor, chosen
 			Name = "RoomRewardPreview",
 			Group = "Combat_UI",
 			DestinationId = exitDoor.ObjectId,
-			OffsetY = modifiedProperties.doorIconOffsetY,
-			OffsetX = modifiedProperties.doorIconOffsetX + doorIconIsometricShiftX,
+			OffsetY = properties.doorIconOffsetY,
+			OffsetX = properties.doorIconOffsetX + doorIconIsometricShiftX,
 			OffsetZ = doorIconOffsetZ + doorIconIsometricShiftZ
 		})
 		SetAlpha({ Id = doorIconId, Fraction = 0.0, Duration = 0.0 })
 		SetAlpha({ Id = doorIconId, Fraction = 1.0, Duration = 0.1 })
 		if skipModifiers[chosenRewardType] == nil or (skipModifiers[chosenRewardType] and not skipModifiers[chosenRewardType].doorIconScale) then
-			SetScale({ Id = doorIconId, Fraction = modifiedProperties.doorIconScale })
+			SetScale({ Id = doorIconId, Fraction = properties.doorIconScale })
 		end
 
 		exitDoor.RewardPreviewIconIds = exitDoor.RewardPreviewIconIds or {}
