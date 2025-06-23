d48bab8fedcf9d469f5211dced27dd6fa6ad7374
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-06-23 11:57:01 +0100

    Fixed dash weapon sound when starting modded run

--- src/Scripts/HubPresentation.lua
+++ src/Scripts/HubPresentation.lua
@@ -45,3 +45,40 @@ modutil.mod.Path.Wrap("UpdateEscapeDoorForLimitGraspShrineUpgrade", function(bas
 	end
 	base(source, args)
 end)
+
+modutil.mod.Path.Wrap("StartNewRunPresentation", function(base, runDoor, args)
+	if runDoor.ModsNikkelMHadesBiomesIsRunStartDoor == true then
+		-- Same as in the base game, but without the dash weapon firing
+		LockCamera({ Id = runDoor.ObjectId, Duration = 1.3, Retarget = true })
+		SetAngle({
+			Id = game.CurrentRun.Hero.ObjectId,
+			Angle = GetAngleBetween({
+				Id = game.CurrentRun.Hero.ObjectId,
+				DestinationId = args.DashTarget or runDoor.ObjectId
+			}),
+			CompleteAngle = true
+		})
+		if game.MapState.FamiliarUnit ~= nil then
+			local familiar = game.MapState.FamiliarUnit
+			if familiar.StartNewRunFunctionName ~= nil then
+				game.thread(game.CallFunctionName, familiar.StartNewRunFunctionName, familiar, args)
+			else
+				SetAnimation({ DestinationId = familiar.ObjectId, Name = familiar.StartNewRunAnimation })
+			end
+		end
+		SetUnitProperty({ DestinationId = game.CurrentRun.Hero.ObjectId, Property = "CollideWithObstacles", Value = false })
+		SetUnitProperty({ DestinationId = game.CurrentRun.Hero.ObjectId, Property = "CollideWithUnits", Value = false })
+		game.SetFixedDashPresentationValues()
+		-- FireWeaponFromUnit({ Weapon = "WeaponBlink", Id = CurrentRun.Hero.ObjectId })
+		game.thread(game.PlayVoiceLines,
+			game.GlobalVoiceLines[args.GlobalVoiceLines] or game.GlobalVoiceLines.StartNewRunVoiceLines)
+		game.wait(0.1)
+		FadeOut({ Duration = 0.3, Color = game.Color.Black })
+		game.EndMusic(game.AudioState.MusicId, game.AudioState.MusicName, 0.5)
+		game.EndAmbience(0.5)
+		game.wait(0.3)
+		game.EndFixedDashPresentationValues()
+	else
+		base(runDoor, args)
+	end
+end)
