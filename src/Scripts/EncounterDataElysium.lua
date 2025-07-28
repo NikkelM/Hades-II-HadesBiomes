c053d7677c70beeced93449393b936afa7233ba2
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-07-28 17:49:23 +0100

    Fixed Minotaur not spawning when loading save

--- src/Scripts/EncounterDataElysium.lua
+++ src/Scripts/EncounterDataElysium.lua
@@ -90,11 +90,10 @@ local encounterModifications = {
 		},
 	},
 	MiniBossMinotaur = {
-		-- Without this, the Minotaur will not spawn if a save is loaded
-		-- TODO: Doesn't always work
-		-- For more consistency, either on the room itself or here invalidate the checkpoint
-		-- Or on room enter, run the preplaced function
-		PreSpawnEnemies = false,
+		InheritFrom = { "MinibossEncounter" },
+		DelayedStart = true,
+		-- Without this, the room exits will immediately unlock after the conversation
+		UnthreadedEvents = EncounterSets.EncounterEventsDefault,
 	},
 
 	-- BOSSES
