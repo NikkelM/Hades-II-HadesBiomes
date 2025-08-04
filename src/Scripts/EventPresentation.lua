32e05e8dabe42532ca20675359b39113372edb74
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-08-04 12:38:06 +0100

    Fixed SingingAnimation not restarting

--- src/Scripts/EventPresentation.lua
+++ src/Scripts/EventPresentation.lua
@@ -9,5 +9,15 @@ modutil.mod.Path.Wrap("FinishedTextLinesPresentation", function(base, source, te
 	if source ~= nil and source.ModsNikkelMHadesBiomesPauseMusicVocalsOnTextLines then
 		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 1, Duration = 0.25 })
 	end
+	-- Custom, for Eurydice we currently use SecretMusicId instead of AmbientMusicId
+	if textLines ~= nil and source.TextLinesPauseSingingFx and game.AudioState.SecretMusicId ~= nil then
+		CreateAnimation({
+			Name = source.SingingFx,
+			DestinationId = source.ObjectId,
+			OffsetX = source.SingingAnimOffsetX or source.AnimOffsetX,
+			OffsetZ = source.AnimOffsetZ,
+			Group = "Combat_UI_World"
+		})
+	end
 	base(source, textLines)
 end)
