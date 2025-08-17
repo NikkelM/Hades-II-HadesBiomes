5a08a86295a09992c4a870b4a93caa7254983f98
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-08-17 22:28:53 +0100

    Upscaled boon icons natively

--- src/Game/Animations/GUI_Boons_VFX.sjson.lua
+++ src/Game/Animations/GUI_Boons_VFX.sjson.lua
@@ -3,72 +3,66 @@ local hadesTwoBoonFile = rom.path.combine(rom.paths.Content(),
 
 -- Make sure to add a Name key!
 
-local addAnimationsParents = {
-	ModsNikkelMHadesBiomesBoonIconSmall = {
-		Name = "ModsNikkelMHadesBiomesBoonIconSmall",
-		InheritFrom = "BoonIcon",
-		Scale = 1.4,
-	},
-}
+local addAnimationsParents = {}
 local addAnimations = {
 	-- #region Eurydice
 	Boon_Eurydice_01 = {
 		Name = "Boon_Eurydice_01",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Eurydice_01",
 	},
 	Boon_Eurydice_02 = {
 		Name = "Boon_Eurydice_02",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Eurydice_02",
 	},
 	Boon_Eurydice_03 = {
 		Name = "Boon_Eurydice_03",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Eurydice_03",
 	},
 	-- #endregion
 	-- #region Patroclus
 	Boon_Patroclus_01 = {
 		Name = "Boon_Patroclus_01",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Patroclus_01",
 	},
 	Boon_Patroclus_02 = {
 		Name = "Boon_Patroclus_02",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Patroclus_02",
 	},
 	Boon_Patroclus_03 = {
 		Name = "Boon_Patroclus_03",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Patroclus_03",
 	},
 	Boon_Patroclus_04 = {
 		Name = "Boon_Patroclus_04",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Patroclus_04",
 	},
 	Boon_Patroclus_05 = {
 		Name = "Boon_Patroclus_05",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Patroclus_05",
 	},
 	-- #endregion
 	-- #region Sisyphus
 	Boon_Sisyphus_01 = {
 		Name = "Boon_Sisyphus_01",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Sisyphus_01",
 	},
 	Boon_Sisyphus_02 = {
 		Name = "Boon_Sisyphus_02",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Sisyphus_02",
 	},
 	Boon_Sisyphus_03 = {
 		Name = "Boon_Sisyphus_03",
-		InheritFrom = "ModsNikkelMHadesBiomesBoonIconSmall",
+		InheritFrom = "BoonIcon",
 		FilePath = "GUIModded\\Screens\\BoonIcons\\Sisyphus_03",
 	},
 	-- #endregion
