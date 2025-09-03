d2c349a981f105dcb21d33c05c52c8b8e4cc31ac
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2025-09-03 11:51:33 +0100

    Fixed NPCs.sjson loading

--- src/Game/Units/NPCs.sjson.lua
+++ src/Game/Units/NPCs.sjson.lua
@@ -3,38 +3,70 @@
 local hadesNPCFile = rom.path.combine(mod.hadesGameFolder, "Content\\Game\\Units\\NPCs.sjson")
 local hadesNPCTable = mod.DecodeSjsonFile(hadesNPCFile)
 
-local hadesTwoNPCFile = rom.path.combine(rom.paths.Content(), "Game\\Units\\NPCs.sjson")
+-- We sometimes get a file-too-small error from H2M with the NPCs.sjson file
+local hadesTwoEnemiesFile = rom.path.combine(rom.paths.Content(), "Game\\Units\\Enemies.sjson")
+
+-- We don't need all NPCs, so only take the ones we actually need
+local hadesNPCsToCopy = {
+	"NPC_Sisyphus_01",
+	"NPC_Eurydice_01",
+	"NPC_Patroclus_01",
+	-- Not using these yet in our NPC logic
+	-- "NPC_Achilles_01",
+	-- "NPC_Achilles_Story_01",
+	-- "NPC_Orpheus_Story_01",
+	-- "NPC_Orpheus_01",
+	"NPC_Persephone_01",
+	"NPC_Hades_Story_02",
+	-- These two are currently still defined in Hades II
+	-- "NPC_Thanatos_01",
+	-- "NPC_Thanatos_Field_01",
+}
+
+local hadesNPCCopySet = {}
+for _, npc in ipairs(hadesNPCTable.Units) do
+	if game.Contains(hadesNPCsToCopy, npc.Name) then
+		table.insert(hadesNPCCopySet, npc)
+	end
+end
+hadesNPCTable.Units = hadesNPCCopySet
 
 -- Modifications/overrides to the Hades NPCs
 -- Use the original names from the Hades file here, as modifications are applied before renaming
-local hadesNPCModifications = {}
+local hadesNPCModifications = {
+	NPC_Sisyphus_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	NPC_Eurydice_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	NPC_Patroclus_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	-- NPC_Achilles_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	-- NPC_Achilles_Story_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	-- NPC_Orpheus_Story_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	-- NPC_Orpheus_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	NPC_Persephone_01 = { InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC", },
+	NPC_Hades_Story_02 = { InheritFrom = "ModsNikkelMHadesBiomes_NPC_Hades_01", },
+}
 local hadesNPCAdditions = {
 	{
-		Name = "ModsNikkelMHadesBiomes_NPC_Cerberus_01",
-		InheritFrom = "BaseNPC",
+		Name = "ModsNikkelMHadesBiomes_NPC_Hades_01",
+		InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC",
 		DisplayInEditor = true,
 		Thing = {
-			AmbientSound = "/SFX/NPCLoops/CerberusGrowlLoop",
 			EditorOutlineDrawBounds = false,
-			Graphic = "Cerberus_HouseIdle",
+			Graphic = "Hades_HouseWritingLoop_IdleLoop",
 			Interact = {
-				Distance = 180.0,
-				OffsetX = -125.0,
-				OffsetY = 50.0,
+				Distance = 220.0,
+				OffsetX = -130.0,
+				OffsetY = 200.0,
 			},
-			Offset = { X = 0, Y = -111 },
 			Points = {
-				{ X = 273,  Y = -128 },
-				{ X = 269,  Y = -181 },
-				{ X = 73,   Y = -250 },
-				{ X = -282, Y = -77 },
-				{ X = -289, Y = 23 },
-				{ X = -160, Y = 78 },
-				{ X = -48,  Y = 76 },
-				{ X = 106,  Y = 7 },
+				{ X = -23,  Y = 129 },
+				{ X = 157,  Y = 33 },
+				{ X = -5,   Y = -43 },
+				{ X = -197, Y = 48 },
 			},
+			SubtitleColor = { Red = 0.83, Green = 0.15, Blue = 0.15 },
 		},
 	},
+	-- The child needs to be before the parent, as the parent is added to the front of the list after the child
 	{
 		Name = "ModsNikkelMHadesBiomes_NPC_Cerberus_Field_01",
 		InheritFrom = "ModsNikkelMHadesBiomes_NPC_Cerberus_01",
@@ -60,8 +92,34 @@ local hadesNPCAdditions = {
 		},
 	},
 	{
+		Name = "ModsNikkelMHadesBiomes_NPC_Cerberus_01",
+		InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC",
+		DisplayInEditor = true,
+		Thing = {
+			AmbientSound = "/SFX/NPCLoops/CerberusGrowlLoop",
+			EditorOutlineDrawBounds = false,
+			Graphic = "Cerberus_HouseIdle",
+			Interact = {
+				Distance = 180.0,
+				OffsetX = -125.0,
+				OffsetY = 50.0,
+			},
+			Offset = { X = 0, Y = -111 },
+			Points = {
+				{ X = 273,  Y = -128 },
+				{ X = 269,  Y = -181 },
+				{ X = 73,   Y = -250 },
+				{ X = -282, Y = -77 },
+				{ X = -289, Y = 23 },
+				{ X = -160, Y = 78 },
+				{ X = -48,  Y = 76 },
+				{ X = 106,  Y = 7 },
+			},
+		},
+	},
+	{
 		Name = "ModsNikkelMHadesBiomes_NPC_Bouldy_01",
-		InheritFrom = "BaseNPC",
+		InheritFrom = "ModsNikkelMHadesBiomes_BaseNPC",
 		DisplayInEditor = true,
 		Thing = {
 			EditorOutlineDrawBounds = false,
@@ -78,6 +136,46 @@ local hadesNPCAdditions = {
 				{ X = 12,   Y = 404, },
 				{ X = 728,  Y = 42, },
 				{ X = 6,    Y = -328, },
+			},
+		},
+	},
+	-- The exact same as BaseNPC, but we need to redefine it as we hook into the Enemies.sjson file where BaseNPC is not yet defined
+	{
+		Name = "ModsNikkelMHadesBiomes_BaseNPC",
+		AutoLockable = false,
+		DisplayInEditor = false,
+		CosmeticRotationSpeed = 280.0,
+		RotationSpeed = 1200.0,
+		Speed = 400.0,
+		Life = {
+			HitFx = "HitSparkB",
+			HomingEligible = false,
+			Invulnerable = true,
+			JumpTargetEligible = false,
+			TriggerOnHit = true,
+		},
+		Thing = {
+			CanBeOccluded = true,
+			EditorOutlineDrawBounds = false,
+			Graphic = "NPCFemaleThinking",
+			Grip = 99999.0,
+			SelectionHeight = 240.0,
+			SelectionShiftY = -30.0,
+			SelectionWidth = 180.0,
+			SpeechCapable = true,
+			UseBoundsForSortDrawArea = true,
+			HighPrioritySfx = true,
+			StopsProjectiles = true,
+			StopsUnits = true,
+			Interact = {
+				Distance = 250.0,
+				OffsetY = -50.0,
+			},
+			Points = {
+				{ X = 0,   Y = 32 },
+				{ X = 64,  Y = 0 },
+				{ X = 0,   Y = -32 },
+				{ X = -64, Y = 0 },
 			},
 		},
 	},
@@ -118,9 +216,10 @@ end
 
 -- Add new NPCs
 for _, npcData in pairs(hadesNPCAdditions) do
-	table.insert(hadesNPCTable.Units, npcData)
+	-- Add to the front, so others can properly inherit from them
+	table.insert(hadesNPCTable.Units, 1, npcData)
 end
 
-sjson.hook(hadesTwoNPCFile, function(data)
+sjson.hook(hadesTwoEnemiesFile, function(data)
 	mod.AddTableKeysSkipDupes(data.Units, hadesNPCTable.Units, "Name")
 end)
