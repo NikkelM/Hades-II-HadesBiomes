8cbe9575a4a5e01411826820086e61af556fdd28
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-10-04 15:34:39 +0100

    Updated CodexText.sjson.lua variable name

--- src/Game/Text/it/CodexText.it.sjson.lua
+++ src/Game/Text/it/CodexText.it.sjson.lua
@@ -4,8 +4,8 @@ local hadesCodexTextTable = mod.DecodeSjsonFile(hadesCodexTextFile)
 hadesCodexTextTable = mod.ModifyCodexTextEntries(hadesCodexTextTable)
 
 -- The Hades II CodexText file is not large enough to handle the hook
-local screenTextFile = rom.path.combine(rom.paths.Content(), "Game\\Text\\it\\HelpText.it.sjson")
-sjson.hook(screenTextFile, function(data)
+local helpTextFile = rom.path.combine(rom.paths.Content(), "Game\\Text\\it\\HelpText.it.sjson")
+sjson.hook(helpTextFile, function(data)
 	-- Shouldn't be finding any duplicates, as we are not hooking into the Hades II Codex file
 	mod.AddTableKeysSkipDupes(data.Texts, hadesCodexTextTable.Texts, "Id")
 end)
