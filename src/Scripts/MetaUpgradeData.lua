391fc7ccdfd17886d70c3d9133a634a19c98ddbd
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-10-27 17:03:57 +0000

    Added Vow of Fangs for Elysium (Closes #113)

--- src/Scripts/MetaUpgradeData.lua
+++ src/Scripts/MetaUpgradeData.lua
@@ -1,7 +1,7 @@
 local nextBiomeShrineBiomeEnemySets = {
 	Tartarus = game.EnemySets.EnemiesBiome2,
-	-- Asphodel = game.EnemySets.EnemiesBiome3,
-	-- Elysium = game.EnemySets.EnemiesBiome4,
+	Asphodel = game.EnemySets.EnemiesBiome3,
+	Elysium = game.EnemySets.EnemiesBiome4,
 }
 
 -- Which enemy is swapped for which other enemy
@@ -13,44 +13,61 @@ local nextbiomeShrineSwapMap = {
 	ThiefMineLayer = { Name = "BloodlessPitcher", },
 	ThiefMineLayerElite = { Name = "BloodlessPitcherElite", },
 	PunchingBagUnit = { Name = "BloodlessNakedBerserker", },
+	PunchingBagUnitElite = { Name = "BloodlessNakedBerserkerElite", },
 	HeavyMelee = { Name = "BloodlessGrenadier", },
+	HeavyMeleeElite = { Name = "BloodlessGrenadierElite", },
 	DisembodiedHand = { Name = "FreezeShotUnit", },
 	DisembodiedHandElite = { Name = "FreezeShotUnitElite", },
 	LightRanged = { Name = "SpreadShotUnit", },
-	HeavyRanged = { Name = "RangedBurrower", },
-	HeavyMeleeElite = { Name = "BloodlessGrenadierElite", },
 	LightRangedElite = { Name = "SpreadShotUnitElite", },
-	PunchingBagUnitElite = { Name = "BloodlessNakedBerserkerElite", },
+	HeavyRanged = { Name = "RangedBurrower", },
 	HeavyRangedElite = { Name = "RangedBurrowerElite", },
 
 	-- Asphodel -> Elysium
+	-- Balancing in the first game doesn't really work for the difficulties we need, so replacing normal units with elites and not touching the previous elites
 	CrusherUnit = { Name = "FlurrySpawner", },
 	LightSpawnerElite = { Name = "FlurrySpawner", },
-	BloodlessNaked = { Name = "ShadeSpearUnit", },
-	BloodlessNakedElite = { Name = "ShadeSpearUnitElite", },
-	BloodlessNakedBerserker = { Name = "ShadeSpearUnit", },
-	BloodlessNakedBerserkerElite = { Name = "ShadeSpearUnitElite", },
-	BloodlessGrenadier = { Name = "ShadeBowUnit", },
-	BloodlessGrenadierElite = { Name = "ShadeBowUnitElite", },
-	BloodlessSelfDestruct = { Name = "ShadeShieldUnit", },
-	BloodlessSelfDestructElite = { Name = "ShadeShieldUnitElite", },
-	BloodlessPitcher = { Name = "ShadeSwordUnit", },
-	BloodlessPitcherElite = { Name = "ShadeSwordUnitElite", },
-	BloodlessWaveFist = { Name = "ShadeBowUnit", },
-	BloodlessWaveFistElite = { Name = "ShadeBowUnitElite", },
-	SpreadShotUnit = { Name = "SplitShotUnit", },
-	SpreadShotUnitElite = { Name = "SplitShotUnitElite", },
-	FreezeShotUnit = { Name = "ChariotSuicide", },
-	FreezeShotUnitElite = { Name = "ChariotSuicideElite", },
-	RangedBurrower = { Name = "Chariot", },
-	RangedBurrowerElite = { Name = "ChariotElite", },
+	BloodlessNaked = { Name = "ShadeSpearUnitElite", },
+	-- BloodlessNakedElite = { Name = "ShadeSpearUnitElite", },
+	BloodlessNakedBerserker = { Name = "ShadeSpearUnitElite", },
+	-- BloodlessNakedBerserkerElite = { Name = "ShadeSpearUnitElite", },
+	BloodlessGrenadier = { Name = "ShadeBowUnitElite", },
+	-- BloodlessGrenadierElite = { Name = "ShadeBowUnitElite", },
+	BloodlessSelfDestruct = { Name = "ShadeShieldUnitElite", },
+	-- BloodlessSelfDestructElite = { Name = "ShadeShieldUnitElite", },
+	BloodlessPitcher = { Name = "ShadeSwordUnitElite", },
+	-- BloodlessPitcherElite = { Name = "ShadeSwordUnitElite", },
+	BloodlessWaveFist = { Name = "ShadeBowUnitElite", },
+	-- BloodlessWaveFistElite = { Name = "ShadeBowUnitElite", },
+	SpreadShotUnit = { Name = "SplitShotUnitElite", },
+	-- SpreadShotUnitElite = { Name = "SplitShotUnitElite", },
+	FreezeShotUnit = { Name = "ChariotSuicideElite", },
+	-- FreezeShotUnitElite = { Name = "ChariotSuicideElite", },
+	RangedBurrower = { Name = "ChariotElite", },
+	-- RangedBurrowerElite = { Name = "ChariotElite", },
 
-	-- TODO: Elysium -> Styx
+	-- Elysium -> Styx
+	-- ShadeSpearUnit = { Name = "SatyrRanged", },
+	-- ShadeSpearUnitElite = { Name = "SatyrRangedElite", },
+	ShadeBowUnit = { Name = "SatyrRanged", },
+	ShadeBowUnitElite = { Name = "SatyrRangedElite", },
+	-- ShadeShieldUnit = { Name = "RatThug", },
+	-- ShadeShieldUnitElite = { Name = "RatThugElite", },
+	ShadeSwordUnit = { Name = "RatThug", },
+	ShadeSwordUnitElite = { Name = "RatThugElite", },
+	ShieldRangedElite = { Name = "HeavyRangedForkedElite", },
+	SplitShotUnit = { Name = "HeavyRangedForked" },
+	SplitShotUnitElite = { Name = "HeavyRangedForkedElite" },
+	Chariot = { Name = "RatThug" },
+	ChariotElite = { Name = "RatThugElite" },
+	-- ChariotSuicide,
+	-- ChariotSuicideElite,
+	FlurrySpawner = { Name = "ThiefImpulseMineLayerElite" },
 }
 
 -- Map the names
 for oldName, newName in pairs(mod.EnemyNameMappings) do
-	mod.UpdatePropertyName(nextbiomeShrineSwapMap, oldName, newName, { }, "nextbiomeShrineSwapMap")
+	mod.UpdatePropertyName(nextbiomeShrineSwapMap, oldName, newName, {}, "nextbiomeShrineSwapMap")
 	mod.UpdateField(nextbiomeShrineSwapMap, oldName, newName, { "Name" }, "nextbiomeShrineSwapMap")
 end
 
