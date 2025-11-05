952a806bbe95bc6f67d6c7b17d870ee6b514da9f
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-11-05 16:47:23 +0000

    Fixed no Ore placement in biomes other than Tartarus, fixed placing ExorcismPoints

--- src/Scripts/HarvestLogic.lua
+++ src/Scripts/HarvestLogic.lua
@@ -5,7 +5,12 @@ modutil.mod.Path.Context.Wrap("SetupHarvestPoints", function(currentRoom, harves
 		modutil.mod.Path.Wrap("GetInactiveIdsByType", function(base, args)
 			if args.Name == "PickaxePoint" or args.Name == "ExorcismPoint" then
 				-- Using Breakables to place the pickaxe and exorcism points in the corners and along walls instead of in the middle of the room
-				return GetIdsByType({ Names = {"Breakable", "BreakableAsphodel", "BreakableElysium", "BreakableStyx", } })
+				return GetIdsByType({ Names = { "Breakable", "BreakableAsphodel", "BreakableElysium", "BreakableStyx" } }) or
+						-- Otherwise, fallback to enemy, points, giving priority to those more likely on the sides of the map
+						GetIdsByType({ Names = { "EnemyPointRanged", "EnemyPointSupport" } }) or
+						GetIdsByType({ Names = { "EnemyPoint", "EnemyPointMelee" } }) or
+						-- If none are found, fallback to Ghost spawn points
+						GetIdsByType({ Names = { "TartarusGhost01", "AsphodelGhost01" } })
 			end
 
 			return base(args)
@@ -13,22 +18,22 @@ modutil.mod.Path.Context.Wrap("SetupHarvestPoints", function(currentRoom, harves
 
 		-- For our modded harvest points, we need to spawn a new obstacle, as we don't use the preset Ids
 		modutil.mod.Path.Wrap("SetupObstacle", function(base, obstacle)
-			local moddedResourceNames = {
+			local moddedBreakableResourceNames = {
 				"ModsNikkelMHadesBiomes_OreTartarus",
 				"ModsNikkelMHadesBiomes_OreAsphodel",
 				"ModsNikkelMHadesBiomes_OreElysium",
 				"ModsNikkelMHadesBiomes_OreStyx",
 			}
 
-			if game.Contains(moddedResourceNames, obstacle.ResourceName) then
+			if game.Contains(moddedBreakableResourceNames, obstacle.ResourceName) or (obstacle.Name == "ExorcismPoint") then
 				-- Additionally, if we are placed on a Breakable ID, we need to break that breakable and those in close proximity to prevent any clipping
 				if obstacle.ModsNikkelMHadesBiomesBreakBreakablesOnPlace then
 					local nearbyBreakables = GetClosestIds({
 						Id = obstacle.ObjectId,
 						DestinationIds = GetIdsByType({
-							Names = { "Breakable", "BlastCubeFused", "BlastCubeFusedRegenerating", },
+							Names = { "Breakable", "BlastCubeFused", "BlastCubeFusedRegenerating", "TartarusGhost01", "AsphodelGhost01" },
 						}),
-						Distance = 70,
+						Distance = 65,
 					})
 					-- Destroy the nearest breakables silently to avoid clipping
 					Destroy({ Ids = nearbyBreakables, Silent = true })
@@ -39,6 +44,12 @@ modutil.mod.Path.Context.Wrap("SetupHarvestPoints", function(currentRoom, harves
 					DestinationId = obstacle.ObjectId,
 					Group = "Standing",
 				})
+
+				-- If it is an ExorcismPoint, angle the ghost towards the nearest spawn point
+				if obstacle.Name == "ExorcismPoint" then
+					local nearestSpawnPoint = GetClosest({ Id = obstacle.ObjectId, DestinationNames = "SpawnPoints", Distance = 200 })
+					AngleTowardTarget({ Id = obstacle.ObjectId, DestinationId = nearestSpawnPoint })
+				end
 			end
 
 			return base(obstacle)
