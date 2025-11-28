aebfa87dccbc69007cda9e6b32766d39528ea6f8
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-11-28 17:43:03 +0000

    Prevent dummy room from creating an encounter

--- src/Scripts/DeathLoopLogic_Late.lua
+++ src/Scripts/DeathLoopLogic_Late.lua
@@ -4,7 +4,10 @@ modutil.mod.Path.Context.Wrap.Static("KillHero", function(victim, triggerArgs)
 			-- Save the actual ending room for when we call EndRun() when starting the next run, so we can still access it there
 			game.CurrentRun.ModsNikkelMHadesBiomesActualCurrentRoom = game.CurrentRun.CurrentRoom
 			-- To prevent an error after uninstalling the mod, due to the room name being unknown
-			game.CurrentRun.CurrentRoom = game.CreateRoom(game.RoomData.BaseRoom)
+			local dummyRoom = game.DeepCopyTable(game.RoomData.BaseRoom) or {}
+			-- Don't CreateRoom(), as that would potentially assign an encounter which would fail to be generated
+			game.RoomInit(dummyRoom)
+			game.CurrentRun.CurrentRoom = dummyRoom
 
 			-- Additionally, to prevent an error with opening the Run History screen after uninstalling the mod,
 			-- we need to encode the ending room name into the KilledByName field, since if it is a modded room name, the game crashes trying to find it
