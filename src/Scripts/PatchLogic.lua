092511a97edcd41aec9b40455fa49fa6e7e243c0
NikkelM <57323886+NikkelM@users.noreply.github.com>
2025-11-29 13:24:05 +0000

    Added patching of previous runs to allow uninstalling without errors

---
+++ src/Scripts/PatchLogic.lua
@@ -0,0 +1,38 @@
+modutil.mod.Path.Wrap("DoPatches", function(base)
+	local moddedRoomNames = {}
+	for _, roomName in pairs(mod.MapFileMappings) do
+		table.insert(moddedRoomNames, roomName)
+	end
+
+	if game.GameState ~= nil then
+		-- #region Fixing any older runs from before the uninstall-error was fixed
+		if not game.IsEmpty(game.GameState.RunHistory) then
+			for _, runData in pairs(GameState.RunHistory) do
+				if game.Contains(moddedRoomNames, runData.EndingRoomName) then
+					runData.VictoryMessage = (runData.VictoryMessage or "") .. "#" .. (runData.EndingRoomName or "")
+					runData.EndingRoomName = nil
+				end
+			end
+		end
+		-- #endregion
+	end
+
+	if game.CurrentRun ~= nil then
+		-- #region If in the Hub and the current run is modded
+		if game.CurrentHubRoom ~= nil and game.Contains(moddedRoomNames, game.CurrentRun.CurrentRoom.Name) then
+			-- Save the actual ending room for when we call EndRun() when starting the next run, so we can still access it there
+			game.CurrentRun.ModsNikkelMHadesBiomesActualCurrentRoomName = game.CurrentRun.CurrentRoom.Name
+			-- To prevent an error after uninstalling the mod, due to the room name being unknown
+			game.CurrentRun.CurrentRoom.Name = "BaseRoom"
+
+			-- Additionally, to prevent an error with opening the Run History screen after uninstalling the mod,
+			-- we need to encode the EndingRoomName into the VictoryMessage field, since if it is a modded room name, the game crashes trying to find it
+			-- This encoding is reversed when opening the Run History screen with the mod installed
+			game.CurrentRun.VictoryMessage = (game.CurrentRun.VictoryMessage or "") .. "#" .. (game.CurrentRun.EndingRoomName or "")
+			game.CurrentRun.EndingRoomName = nil
+		end
+	end
+	-- #endregion
+
+	base()
+end)
--- src/main.lua
+++ src/main.lua
@@ -373,6 +373,7 @@ local function on_ready()
 		import "Scripts/HubPresentation.lua"
 		import "Scripts/NarrativeData.lua"
 		import "Scripts/NarrativeLogic.lua"
+		import "Scripts/PatchLogic.lua"
 		import "Scripts/PowersLogic.lua"
 		import "Scripts/RequirementsLogic.lua"
 		import "Scripts/RewardPresentation.lua"
