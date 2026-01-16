25907f5ec8eb5961d99c0c7ff306eae2eca9bab4
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2026-01-16 22:39:55 +0000

    Updated DoPatches to only run necessary operations

--- src/Scripts/PatchLogic.lua
+++ src/Scripts/PatchLogic.lua
@@ -1,7 +1,14 @@
 modutil.mod.Path.Wrap("DoPatches", function(base)
 	if game.GameState ~= nil then
+		-- IMPORTANT: Whenever this function is changed, increase the PatchRevision number at the end of this function
+		-- All required checks will run once with the previous GameState's PatchRevision number, and then the PatchRevision number will be updated to the latest one
+		game.GameState.ModsNikkelMHadesBiomesPatchRevision = game.GameState.ModsNikkelMHadesBiomesPatchRevision or 0
+		mod.DebugPrint(
+			"Applying patches to GameState, current patch revision: " .. game.GameState.ModsNikkelMHadesBiomesPatchRevision, 4)
+
+		-- #region RunHistory
 		if not game.IsEmpty(game.GameState.RunHistory) then
-			if not game.GameState.ModsNikkelMHadesBiomesPatchedEndingRoomNames then
+			if game.GameState.ModsNikkelMHadesBiomesPatchRevision < 1 then
 				for _, runData in pairs(game.GameState.RunHistory) do
 					if mod.WasModdedRun(runData) then
 						-- #region Fix any older runs from before the uninstall-error was fixed
@@ -33,55 +40,78 @@ modutil.mod.Path.Wrap("DoPatches", function(base)
 					end
 				end
 			end
-			game.GameState.ModsNikkelMHadesBiomesPatchedEndingRoomNames = true
-			-- Any other changes to RunHistory must be made in a separate loop below, as GameState.ModsNikkelMHadesBiomesPatchedEndingRoomNames will have been set already
-		end
-
-		-- #region Initialize GameState variables
-		game.GameState.ModsNikkelMHadesBiomesCompletedRunsCache = game.GameState.ModsNikkelMHadesBiomesCompletedRunsCache or
-				0
-		game.GameState.ModsNikkelMHadesBiomesClearedRunsCache = game.GameState.ModsNikkelMHadesBiomesClearedRunsCache or 0
-		game.GameState.ModsNikkelMHadesBiomesHighestShrinePointClearModdedRunCache = game.GameState
-				.ModsNikkelMHadesBiomesHighestShrinePointClearModdedRunCache or 0
-		game.GameState.ModsNikkelMHadesBiomesFastestModdedRunClearTimeCache = game.GameState
-				.ModsNikkelMHadesBiomesFastestModdedRunClearTimeCache or 999999
-		-- #endregion
-
-		-- #region Patch GiftTextLineSets
-		-- If ThanatosGift04 has already been completed, move it to ThanatosGift04_B, since that is now the only one still available
-		if game.GameState.TextLinesRecord["ThanatosGift04"] ~= nil then
-			game.GameState.TextLinesRecord["ThanatosGift04"] = nil
-			game.GameState.TextLinesRecord["ThanatosGift04_B"] = true
 		end
 		-- #endregion
 
-		-- #region Retroactively award additional Nightmare for modded testaments that were completed before the rewards were increased
-		if not game.GameState.ModsNikkelMHadesBiomesPatchedTestamentsIncreasedRewards then
-			mod.PatchTestamentIncreasedRewards()
-			game.GameState.ModsNikkelMHadesBiomesPatchedTestamentsIncreasedRewards = true
+		-- #region Misc GameState
+		if game.GameState.ModsNikkelMHadesBiomesPatchRevision < 1 then
+			-- #region Initialize GameState variables
+			game.GameState.ModsNikkelMHadesBiomesCompletedRunsCache = game.GameState.ModsNikkelMHadesBiomesCompletedRunsCache or
+					0
+			game.GameState.ModsNikkelMHadesBiomesClearedRunsCache = game.GameState.ModsNikkelMHadesBiomesClearedRunsCache or 0
+			game.GameState.ModsNikkelMHadesBiomesHighestShrinePointClearModdedRunCache = game.GameState
+					.ModsNikkelMHadesBiomesHighestShrinePointClearModdedRunCache or 0
+			game.GameState.ModsNikkelMHadesBiomesFastestModdedRunClearTimeCache = game.GameState
+					.ModsNikkelMHadesBiomesFastestModdedRunClearTimeCache or 999999
+			game.GameState.ModsNikkelMHadesBiomesCodexEntriesUnlockedCache = game.GameState
+					.ModsNikkelMHadesBiomesCodexEntriesUnlockedCache or 0
+			-- #endregion
+
+			-- #region Patch GiftTextLineSets
+			-- If ThanatosGift04 has already been completed, move it to ThanatosGift04_B, since that is now the only one still available
+			if game.GameState.TextLinesRecord["ThanatosGift04"] ~= nil then
+				game.GameState.TextLinesRecord["ThanatosGift04"] = nil
+				game.GameState.TextLinesRecord["ThanatosGift04_B"] = true
+			end
+			-- #endregion
+
+			-- #region Retroactively award additional Nightmare for modded testaments that were completed before the rewards were increased
+			if not game.GameState.ModsNikkelMHadesBiomesPatchedTestamentsIncreasedRewards then
+				mod.PatchTestamentIncreasedRewards()
+				game.GameState.ModsNikkelMHadesBiomesPatchedTestamentsIncreasedRewards = true
+			end
+			-- #endregion
+
+			-- #region Patch missing Codex unlocks in CodexStatus
+			for entryName, _ in pairs(game.CodexData[mod.CodexChapterName].Entries) do
+				game.CheckCodexUnlock(mod.CodexChapterName, entryName)
+			end
+			-- #endregion
+		end
+
+		-- TODO: From patch version 1 onwards we don't need to check this anymore and should remove the unnecessary key/if-branch above
+		if game.GameState.ModsNikkelMHadesBiomesPatchRevision >= 1 then
+			game.GameState.ModsNikkelMHadesBiomesPatchedTestamentsIncreasedRewards = nil
 		end
 		-- #endregion
+
+		-- #region CurrentRun
+		if game.CurrentRun ~= nil then
+			-- #region If in the Hub and the current run is modded
+			if game.GameState.ModsNikkelMHadesBiomesPatchRevision < 1 then
+				if game.CurrentHubRoom ~= nil and game.Contains(mod.ModdedRoomNamesWithOldElysium, game.CurrentRun.CurrentRoom.Name) then
+					-- Save the actual ending room for when we call EndRun() when starting the next run, so we can still access it there
+					game.CurrentRun.ModsNikkelMHadesBiomesActualCurrentRoomName = game.CurrentRun.CurrentRoom.Name
+					-- To prevent an error after uninstalling the mod, due to the room name being unknown
+					game.CurrentRun.CurrentRoom.Name = "BaseRoom"
+
+					-- Additionally, to prevent an error with opening the Run History screen after uninstalling the mod,
+					-- we need to encode the EndingRoomName into the VictoryMessage field, since if it is a modded room name, the game crashes trying to find it
+					-- This encoding is reversed when opening the Run History screen with the mod installed
+					game.CurrentRun.VictoryMessage = (game.CurrentRun.VictoryMessage or "") ..
+							"#" .. (game.CurrentRun.EndingRoomName or "")
+					game.CurrentRun.EndingRoomName = nil
+				end
+			end
+			-- #endregion
+		end
+		-- #endregion
+
+		-- IMPORTANT: This must be incremented every time this function is changed
+		game.GameState.ModsNikkelMHadesBiomesPatchRevision = 1
 	end
 
-	if game.CurrentRun ~= nil then
-		-- #region If in the Hub and the current run is modded
-		if game.CurrentHubRoom ~= nil and game.Contains(mod.ModdedRoomNamesWithOldElysium, game.CurrentRun.CurrentRoom.Name) then
-			-- Save the actual ending room for when we call EndRun() when starting the next run, so we can still access it there
-			game.CurrentRun.ModsNikkelMHadesBiomesActualCurrentRoomName = game.CurrentRun.CurrentRoom.Name
-			-- To prevent an error after uninstalling the mod, due to the room name being unknown
-			game.CurrentRun.CurrentRoom.Name = "BaseRoom"
-
-			-- Additionally, to prevent an error with opening the Run History screen after uninstalling the mod,
-			-- we need to encode the EndingRoomName into the VictoryMessage field, since if it is a modded room name, the game crashes trying to find it
-			-- This encoding is reversed when opening the Run History screen with the mod installed
-			game.CurrentRun.VictoryMessage = (game.CurrentRun.VictoryMessage or "") ..
-					"#" .. (game.CurrentRun.EndingRoomName or "")
-			game.CurrentRun.EndingRoomName = nil
-		end
-	end
-	-- #endregion
-
-	base()
+	return base()
 end)
 
 function mod.PatchTestamentIncreasedRewards()
