33ea65eff2ad42205d183162787453ebd3f069a4
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2026-01-20 14:00:01 +0100

    Do not skip the run clear screen if the Chaos Trial allows to show it (#518)

--- src/Scripts/FunctionMappings/BossStyx.lua
+++ src/Scripts/FunctionMappings/BossStyx.lua
@@ -969,7 +969,9 @@ function mod.RevulnerablePlayerAfterShout()
 end
 
 function mod.ModsNikkelMHadesBiomesOpenRunClearScreen()
-	if game.CurrentRun.ActiveBounty ~= nil then
+	local bountyId = game.CurrentRun.ActiveBounty
+	local bountyData = bountyId and game.BountyData[bountyId]
+	if bountyData and not bountyData.ModsNikkelMHadesBiomesForceRunClearScreen then
 		-- This is a Chaos Trial run
 		return
 	end
