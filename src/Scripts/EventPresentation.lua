555b825b764bca63e6879cfdec6ed5a8d71d7fcb
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2026-02-16 14:42:16 +0000

    Fixed vocals soundcue modifications on textline start

--- src/Scripts/EventPresentation.lua
+++ src/Scripts/EventPresentation.lua
@@ -2,6 +2,17 @@ modutil.mod.Path.Wrap("StartedTextLinesPresentation", function(base, source, tex
 	if source ~= nil and source.ModsNikkelMHadesBiomesPauseMusicVocalsOnTextLines then
 		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 0 })
 	end
+	if source ~= nil and source.ModsNikkelMHadesBiomesPauseMusicVocals2OnTextLines then
+		SetSoundCueValue({ Names = { "Vocals2", }, Id = game.AudioState.SecretMusicId, Value = 0 })
+	end
+	if textLines ~= nil and textLines.ModsNikkelMHadesBiomesPauseMusicVocalsOnTextLines then
+		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 0 })
+		SetSoundCueValue({ Names = { "Vocals2", }, Id = game.AudioState.SecretMusicId, Value = 1 })
+	end
+	if textLines ~= nil and textLines.ModsNikkelMHadesBiomesPauseMusicVocals2OnTextLines then
+		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 1 })
+		SetSoundCueValue({ Names = { "Vocals2", }, Id = game.AudioState.SecretMusicId, Value = 0 })
+	end
 
 	return base(source, textLines)
 end)
@@ -9,6 +20,17 @@ end)
 modutil.mod.Path.Wrap("FinishedTextLinesPresentation", function(base, source, textLines)
 	if source ~= nil and source.ModsNikkelMHadesBiomesPauseMusicVocalsOnTextLines then
 		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 1, Duration = 0.25 })
+	end
+	if source ~= nil and source.ModsNikkelMHadesBiomesPauseMusicVocals2OnTextLines then
+		SetSoundCueValue({ Names = { "Vocals2", }, Id = game.AudioState.SecretMusicId, Value = 1, Duration = 0.25 })
+	end
+	if textLines ~= nil and textLines.ModsNikkelMHadesBiomesPauseMusicVocalsOnTextLines then
+		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 1, Duration = 0.25 })
+		SetSoundCueValue({ Names = { "Vocals2", }, Id = game.AudioState.SecretMusicId, Value = 1 })
+	end
+	if textLines ~= nil and textLines.ModsNikkelMHadesBiomesPauseMusicVocals2OnTextLines then
+		SetSoundCueValue({ Names = { "Vocals", }, Id = game.AudioState.SecretMusicId, Value = 1 })
+		SetSoundCueValue({ Names = { "Vocals2", }, Id = game.AudioState.SecretMusicId, Value = 1, Duration = 0.25 })
 	end
 	-- Custom, for Eurydice we currently use SecretMusicId instead of AmbientMusicId
 	if textLines ~= nil and source.TextLinesPauseSingingFx and game.AudioState.SecretMusicId ~= nil then
