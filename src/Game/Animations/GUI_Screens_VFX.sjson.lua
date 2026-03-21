581bf7e5c57aaf50fc6f22d87ccff692e6f60615
NikkelM <57323886+NikkelM@users.noreply.github.com>
2026-03-21 16:37:24 +0000

    Show main menu logo earlier

--- src/Game/Animations/GUI_Screens_VFX.sjson.lua
+++ src/Game/Animations/GUI_Screens_VFX.sjson.lua
@@ -2,13 +2,72 @@ local hadesTwoScreensVFXFile = rom.path.combine(rom.paths.Content(), "Game\\Anim
 
 local hadesTwoScreenVFXModifications = {
 	-- Change the ChildAnimation to the X, so that it gets rendered below the two logos
+	MainMenuIn = {
+		ChildAnimation = "ModsNikkelMHadesBiomes_MainMenuDelay",
+	},
 	MainMenuLoop = {
 		ChildAnimation = "ModsNikkelMHadesBiomes_MainMenuMashupX",
+	},
+	-- The actual fade is on the _Early variant, this one should simply replace one the Loop starts
+	MainMenuLogoOutline = {
+		StartScale = 0.6825999,
+		StartAlpha = 1,
+		Duration = 0,
 	},
 }
 
 -- These need to be added before the others, as they are inherited from
-local addParentAnimations = {}
+local addParentAnimations = {
+	ModsNikkelMHadesBiomes_MainMenuMashupX_Early = {
+		Name = "ModsNikkelMHadesBiomes_MainMenuMashupX_Early",
+		FilePath = "NikkelM-HadesBiomesMainMenu\\Screens\\Logo\\MashupX",
+		StartScale = 0.5,
+		EndScale = 0.505,
+		OffsetX = -1051,
+		OffsetY = -754.5,
+		Duration = 0.5,
+		Loop = false,
+		HoldLastFrame = true,
+		StartAlpha = 0,
+		EndAlpha = 1,
+		EaseIn = 0,
+		EaseOut = 1,
+		FlipHorizontal = true,
+		ChildAnimation = "ModsNikkelMHadesBiomes_MainMenuMashupLogo_Early",
+	},
+	ModsNikkelMHadesBiomes_MainMenuMashupLogo_Early = {
+		Name = "ModsNikkelMHadesBiomes_MainMenuMashupLogo_Early",
+		FilePath = "NikkelM-HadesBiomesMainMenu\\Screens\\Logo\\HadesLogo",
+		StartScale = 0.55,
+		EndScale = 0.56,
+		OffsetX = -820,
+		OffsetY = -747,
+		Duration = 0.5,
+		Loop = false,
+		HoldLastFrame = true,
+		StartAlpha = 0,
+		EndAlpha = 1,
+		EaseIn = 0,
+		EaseOut = 1,
+		-- The original H2 outline/logo
+		ChildAnimation = "MainMenuLogoOutline_Early",
+	},
+	MainMenuLogoOutline_Early = {
+		Name = "MainMenuLogoOutline_Early",
+		FilePath = "Screens\\MainMenu\\HadesLogoOutline",
+		StartScale = 0.6825,
+		EndScale = 0.6826,
+		OffsetX = -1443.5,
+		OffsetY = -762.5,
+		Duration = 0.5,
+		Loop = false,
+		HoldLastFrame = true,
+		StartAlpha = 0,
+		EndAlpha = 1,
+		EaseIn = 0,
+		EaseOut = 1,
+	},
+}
 
 local addAnimations = {
 	-- #region Run End Screens
@@ -147,17 +206,23 @@ local addAnimations = {
 	},
 	-- #endregion
 	-- #region Main Menu
+	-- Dummy animation that's only there to delay the logo fade-in
+	ModsNikkelMHadesBiomes_MainMenuDelay = {
+		Name = "ModsNikkelMHadesBiomes_MainMenuDelay",
+		ChainTo = "ModsNikkelMHadesBiomes_MainMenuMashupX_Early",
+		Duration = 1.7,
+	},
 	ModsNikkelMHadesBiomes_MainMenuMashupX = {
 		Name = "ModsNikkelMHadesBiomes_MainMenuMashupX",
 		FilePath = "NikkelM-HadesBiomesMainMenu\\Screens\\Logo\\MashupX",
-		StartScale = 0.5,
+		StartScale = 0.5049999,
 		EndScale = 0.505,
 		OffsetX = -180,
 		OffsetY = -425,
-		Duration = 0.5,
+		Duration = 0,
 		Loop = false,
 		HoldLastFrame = true,
-		StartAlpha = 0,
+		StartAlpha = 1,
 		EndAlpha = 1,
 		EaseIn = 0,
 		EaseOut = 1,
@@ -167,14 +232,14 @@ local addAnimations = {
 	ModsNikkelMHadesBiomes_MainMenuMashupLogo = {
 		Name = "ModsNikkelMHadesBiomes_MainMenuMashupLogo",
 		FilePath = "NikkelM-HadesBiomesMainMenu\\Screens\\Logo\\HadesLogo",
-		StartScale = 0.55,
+		StartScale = 0.5599999,
 		EndScale = 0.56,
 		OffsetX = 250,
 		OffsetY = -370,
-		Duration = 0.5,
+		Duration = 0,
 		Loop = false,
 		HoldLastFrame = true,
-		StartAlpha = 0,
+		StartAlpha = 1,
 		EndAlpha = 1,
 		EaseIn = 0,
 		EaseOut = 1,
