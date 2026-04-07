79b3192fcb4c4aa9c9428a0f52a9b5e3652b5b35
NikkelM <57323886+NikkelM@users.noreply.github.com>
2026-04-07 17:35:47 +0100

    Fixed nil error on room reward spawn

--- src/Scripts/ResourcePresentation.lua
+++ src/Scripts/ResourcePresentation.lua
@@ -1,6 +1,6 @@
 modutil.mod.Path.Wrap("RoomRewardSpawnPresentation", function(base, reward, args)
 	-- For the Chaos boon on the first modded run, force the conversation to play
-	if game.CurrentRun.ModsNikkelMHadesBiomesIsModdedRun and game.CurrentRun.CurrentRoom.Reward.ModsNikkelMHadesBiomesDisableGoldConversionEligible then
+	if game.CurrentRun.ModsNikkelMHadesBiomesIsModdedRun and game.CurrentRun.CurrentRoom and game.CurrentRun.CurrentRoom.Reward and game.CurrentRun.CurrentRoom.Reward.ModsNikkelMHadesBiomesDisableGoldConversionEligible then
 		reward.GoldConversionEligible = false
 	end
 
